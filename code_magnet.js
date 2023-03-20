d415b28bd0065723031114200928381e78c1af19
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-20 16:52:24 +0300

    First commit

---
+++ code_magnet.js
@@ -0,0 +1,9 @@
+//CODE_MAGNET
+var name = "MUKONGE";
+var i = 0;
+while (i < 2) {
+    document.write("Happy Earthday to you");
+    i = i + 1;
+}
+document.write("Happy Earthday dear" + name + "<br>");
+document.write("Happy Earthday to you.<br>");
